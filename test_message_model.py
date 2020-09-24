a050360420f582b70cfb3fe9206aaecb63a63d0a
Jeremy Langevin <langevinj832@gmail.com>
2020-09-24 10:26:12 -0400

    clean up and overhaul test_message_model.py

--- test_message_model.py
+++ test_message_model.py
@@ -1,14 +1,9 @@
 """Message model tests."""
-
-# run these tests like:
-#
-#    python -m unittest test_user_model.py
-
-
-from app import app
 import os
 from unittest import TestCase
-from models import db, User, Message, Follows
+from sqlalchemy import exc
+from models import db, User, Message, Follows, Likes
+
 
 
 # BEFORE we import our app, let's set an environmental variable
@@ -20,77 +15,107 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
 
 # Now we can import app
 
-db.create_all()
-
+from app import app
 
 class MessageModelTestCase(TestCase):
     """Test model for Messages."""
 
     def setUp(self):
-        """Create test client, add sample data."""
+        """Create test client, add sample data."""  
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        db.drop_all()
+        db.create_all()
+
+        self.uid = 12345
+        u = User.signup("testtest", "email@email.com", "password", None)
+        u.id = self.uid 
+        db.session.commit()
+
+        self.u = User.query.get(self.uid)
 
         self.client = app.test_client()
 
+    def tearDown(self):
+        res = super().tearDown()
+        db.session.rollback()
+        return res
+
+    ########
+    #Test message creation
+    ###
+     
     def test_user_model(self):
         """Does basic model work?"""
-
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
-
-        db.session.add(u)
-        db.session.commit()
 
         m = Message(
             text="I'm tired today",
             timestamp="2007-05-08 12:35:29.123",
-            user_id=u.id
+            user_id=self.u.id
         )
-
         db.session.add(m)
         db.session.commit()
 
         #User should have 1 message
-        self.assertEqual(len(u.messages), 1)
+        self.assertEqual(len(self.u.messages), 1)
+        self.assertEqual(m.user_id, self.u.id)
 
-    def test_message_create_failure(self):
-        """Test Message.create fail to creates a message when invliad credentials are given"""
 
-        #before creating user?
-
-        #incorrect user id
-
-        #no give a mandatory creendtial
-
-        #given a string that is too long
-
-    def test_message_user_relationship(self):
-        """Test that the relationship between Message and User is active"""
-
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
-
-        db.session.add(u)
-        db.session.commit()
-
+    def test_too_long_message(self):
         m = Message(
-            text="I'm tired today",
+            text="asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfaddfasddfasdadfasdfasdfasdfasdfasdfasfasdasdfasdasdasdasdfasdfasdfasdfasdfasdfasdfasdfdasdfasdfasdfasdasdfasdfasdasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdasdfasdfsdfsdfsadfasdfasfasdfasfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfsadfasdfasfgasdfsdfsdfsdfsdf",
             timestamp="2007-05-08 12:35:29.123",
-            user_id=u.id
+            user_id=self.u.id
         )
+        
+        db.session.add(m)
+        with self.assertRaises(exc.DataError) as context:
+            db.session.commit()
+    
+    def test_no_text(self):
+        m = Message(text=None, timestamp="2007-05-08 12:35:29.123", user_id=self.u.id)
+        db.session.add(m)
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
+    
+    def test_invalid_timestamp(self):
+        mid= 5555
+        m = Message(
+            text="Testing testing 123",
+            timestamp=None,
+            user_id=self.u.id
+        )
+        m.id = mid
+
+        m2 = Message(
+            text="testing testing 1234",
+            timestamp="12 34 45",
+            user_id=self.u.id 
+        )      
 
         db.session.add(m)
         db.session.commit()
+        find_m = Message.query.get(m.id)
+        self.assertEqual(m, find_m)
 
-        self.assertEqual(m.user, u)
+        db.session.add(m2)
+        with self.assertRaises(exc.DataError) as context:
+            db.session.commit()
+        
+    def test_message_user_relationship(self):
+        m = Message(
+            text="Im tired today",
+            timestamp="2007-05-08 12:35:29.123",
+            user_id=self.u.id
+        )
 
+        mid = 123456
+        m.id = mid 
+        db.session.add(m)
+        db.session.commit()
+
+        message = Message.query.get(mid)
+
+        self.assertEqual(message.user_id, self.u.id)
+        self.assertEqual(message.user, self.u)
+  
     
